94fef5f52a7a7b0c58b58bc1cf1183f09186618a
jekaslav <evgenevgen8585@gmail.com>
2023-03-27 21:25:41 +0400

    Initial commit without pagination

---
+++ TotalCommander/FileManager.cs
@@ -0,0 +1,237 @@
+﻿namespace TotalCommander;
+
+public class FileManager
+{
+    public void CreateDir()
+    {
+        Console.WriteLine("Enter a directory name:");
+        var newDir = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(newDir) || Path.GetInvalidFileNameChars()
+                .Any(c => newDir.Contains(c)))
+        {
+            throw new Exception("Incorrect");
+        }
+        
+        GetPath();
+        
+        Directory.CreateDirectory(newDir);
+
+        Console.WriteLine($"Directory {newDir} is created.");
+    }
+
+    public void RemoveDir()
+    {
+        Console.WriteLine("Enter a directory name:");
+        var rmNameDir = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(rmNameDir) || Path.GetInvalidFileNameChars()
+                .Any(c => rmNameDir.Contains(c)))
+        {
+            throw new Exception("Incorrect");
+        }
+        
+        GetPath();
+        
+        if (Directory.Exists(rmNameDir))
+        {
+            Directory.Delete(rmNameDir);
+        }
+        else
+        {
+            throw new Exception("error");
+        }
+
+        Console.WriteLine($"Directory {rmNameDir} is removed.");
+    }
+
+    public void RemoveFile()
+    {
+        Console.WriteLine("Enter a file name:");
+        var rmNameFile = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(rmNameFile) || Path.GetInvalidFileNameChars()
+                .Any(c => rmNameFile.Contains(c)))
+        {
+            throw new Exception("Incorrect");
+        }
+        
+        GetPath();
+        
+        if (File.Exists(rmNameFile))
+        {
+            File.Delete(rmNameFile);
+        }
+        else
+        {
+            throw new Exception("where is the file?");
+        }
+
+        Console.WriteLine($"File {rmNameFile} is removed.");
+    }
+    
+    public void CopyFile()
+    {
+        Console.WriteLine("Enter a file name in current directory:");
+        var copyName = Console.ReadLine();
+    
+        if (string.IsNullOrWhiteSpace(copyName) || Path.GetInvalidPathChars()
+                .Any(c => copyName.Contains(c)))
+        {
+            throw new Exception("Incorrect path to the file.");
+        }
+        
+        Console.WriteLine("Enter a target name in the new directory for the file:");
+        var trgName = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(trgName) || Path.GetInvalidPathChars()
+                .Any(c => copyName.Contains(c)))
+        {
+            throw new Exception("Incorrect path to the file.");
+        }
+    
+        if (File.Exists(copyName) || Directory.Exists(trgName))
+        {
+            File.Copy( GetPath() + copyName,  "C:\\" + trgName + "\\" + Path.GetFileName(copyName));
+        }
+        else
+        {
+            throw new Exception("where is the file?");
+        }
+    
+        Console.WriteLine($"File {copyName} is copied");
+    }
+
+    public void CopyDir()
+    {
+        Console.WriteLine("enter source dir:");
+        var sPath = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(sPath) || Path.GetInvalidPathChars()
+                .Any(c => sPath.Contains(c)))
+        {
+            throw new Exception("Incorrect path.");
+        }
+
+        Console.WriteLine("enter target dir:");
+        var tPath = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(tPath) || Path.GetInvalidPathChars()
+                .Any(c => tPath.Contains(c)))
+        {
+            throw new Exception("Incorrect path.");
+        }
+
+        var sDir = new DirectoryInfo(GetPath() + sPath); // нужно создавать так экземпляры класса или обращаться напрямую, когда используем в циклах?
+        var tDir = new DirectoryInfo("C:\\" + tPath + "\\" + Path.GetFileName(sPath));
+
+        if (tDir.Exists)
+        {
+            tDir.Create();
+        }
+
+        foreach (var file in sDir.GetFiles())
+        {
+            file.CopyTo(Path.Combine(tDir.FullName, file.Name), true);
+        }
+
+        foreach (var subDir in sDir.GetDirectories())                // подпапки
+        {
+            var targetSubDir = tDir.CreateSubdirectory(subDir.Name);    
+            CopyFolderRecursive(subDir, targetSubDir);
+        }
+        
+        Console.WriteLine($"File {sPath} is copied");
+    }
+
+    public void MoveFile()
+    {
+        Console.WriteLine("enter a file name:");
+        var fName = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(fName) || Path.GetInvalidFileNameChars()
+                .Any(c => fName.Contains(c)))
+        {
+            throw new Exception("Incorrect");
+        }
+        
+        Console.WriteLine("enter a target path:");
+        var tPath = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(tPath) || Path.GetInvalidPathChars()
+                .Any(c => tPath.Contains(c)))
+        {
+            throw new Exception("Incorrect path to the file.");
+        }
+
+        if (File.Exists(fName))
+        {
+            File.Move(fName, "C:\\" + tPath + "\\" + Path.GetFileName(fName), true);
+        }
+        else
+        {
+            throw new FileNotFoundException("where is the file");
+        }
+    }
+
+    public void MoveDir()
+    {
+        Console.WriteLine("Enter the directory name:");
+        var dName = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(dName) || Path.GetInvalidFileNameChars()
+                .Any(c => dName.Contains(c)))
+        {
+            throw new Exception("Incorrect");
+        }
+        
+        Console.WriteLine("Enter a target path for the directory:");
+        var tPath = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(tPath) || Path.GetInvalidPathChars()
+                .Any(c => tPath.Contains(c)))
+        {
+            throw new Exception("Incorrect path.");
+        }
+
+        if (Directory.Exists(dName))
+        {
+            Directory.Move(GetPath() + dName, "C:\\" + tPath + "\\" + Path.GetFileName(dName));
+        }
+        else
+        {
+            throw new DirectoryNotFoundException("where is the directory");
+        }
+        
+        var sDir = new DirectoryInfo(GetPath() + dName);
+        var tDir = new DirectoryInfo("C:\\" + tPath + "\\" + Path.GetFileName(tPath));
+        
+        foreach (var subDir in sDir.GetDirectories())
+        {
+            var targetSubDir = tDir.CreateSubdirectory(subDir.Name);
+            CopyFolderRecursive(subDir, targetSubDir);
+        }
+    }
+    
+    private static void CopyFolderRecursive(DirectoryInfo source, DirectoryInfo target)
+    {
+        // Copy all files
+        foreach (var file in source.GetFiles())
+        {
+            file.CopyTo(Path.Combine(target.FullName, file.Name), true);
+        }
+
+        // Copy all subfolders
+        foreach (var subfolder in source.GetDirectories())
+        {
+            var targetSubfolder = target.CreateSubdirectory(subfolder.Name);
+            CopyFolderRecursive(subfolder, targetSubfolder);
+        }
+    }
+    
+    private string GetPath()
+    {
+        var currPath = Directory.GetCurrentDirectory() + "\\";
+        return currPath;
+    }
+}
---
+++ TotalCommander/FilesDirectory.cs
@@ -0,0 +1,94 @@
+﻿namespace TotalCommander;
+
+public class FilesDirectory
+{
+    private const string PathToDocuments = "C:\\";
+    public void ViewDirectory()
+    {
+        var files = Directory.GetFiles(PathToDocuments);
+        var directories = Directory.GetDirectories(PathToDocuments);
+
+        Console.WriteLine("Files:");
+        foreach (var file in files)
+        {
+            Console.WriteLine(file);
+        }
+
+        Console.WriteLine("\nDirectories:");
+        foreach (var directory in directories)
+        {
+            Console.WriteLine(directory);
+        }
+    }
+    
+    public void GoToOtherDirectory()
+    {
+        Console.WriteLine("Enter a directory path:");
+        var path = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(path) || Path.GetInvalidPathChars()
+                .Any(c => path.Contains(c)))
+        {
+            throw new Exception("Incorrect path.");
+        }
+
+        if (Directory.Exists(PathToDocuments + path))
+        {
+            Directory.SetCurrentDirectory(PathToDocuments + path);
+            Console.WriteLine($"Current directory is now {PathToDocuments + path}");
+            
+            var files = Directory.GetFiles(PathToDocuments + path);
+            var directories = Directory.GetDirectories(PathToDocuments + path);
+
+            Console.WriteLine("Files:");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            Console.WriteLine("\nDirectories:");
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Directory does not exist");
+        }
+    }
+    
+    public void FileViewInfo()
+    {
+        Console.WriteLine("enter name");
+        var fileName = Console.ReadLine();
+        var fileInfo = new FileInfo(fileName!); // нужно создавать так экземпляры класса или обращаться напрямую, когда используем в циклах?
+        if (fileInfo.Exists)
+        {
+            Console.WriteLine("File name:" + fileInfo.Name);
+            Console.WriteLine("Size:" + fileInfo.Length + "bytes");
+            Console.WriteLine("Created:" + fileInfo.CreationTime);
+            Console.WriteLine("Last modified:" + fileInfo.LastWriteTime);
+            Console.WriteLine();
+        }
+    }
+    
+    public void Help()
+    {
+        Console.WriteLine
+        (@"
+        1. dir - вывод списка файлов и папок в текущей директории
+        2. cd  - переход в указанную директорию
+        3. mkdir - создание новой папки с указанным именем
+        4. rmdir - удаление папки с указанным именем
+        5. rmfile - удаление файла с указанным именем
+        6. copydir - копирование папки из одного места в другое
+        7. copyfile - копирование файла из одного места в другое
+        8. movefile - перемещение файла из одного места в другое
+        9. movedir - перемещение папки из одного места в другое
+        10. info - получение информации о файле
+        11. help - вывод списка доступных команд и их описания
+        12. exit q - выход из программы");
+        
+    }
+}
